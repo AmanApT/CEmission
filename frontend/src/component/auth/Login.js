ad2c64da2d22436f983d84ea43446311837732b3
AmanApT <amanasstudent@gmail.com>
2023-02-11 12:48:26 +0530

    Login Page in Development

--- frontend/src/component/auth/Login.js
+++ frontend/src/component/auth/Login.js
@@ -1,7 +1,25 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="loginAuth">
+      <div className="loginDialog">
+        <h1 className="loginTitle">WELCOME BACK</h1>
+        <div className="authInput">
+        <TextField id="outlined-basic" label="Email" color="success" variant="filled" />
+        </div>
+        <div className="authInput">
+        <TextField id="outlined-basic" label="Enter Password" color="success" variant="outlined" />
+        </div>
+      <button> Login</button>
+      <div>
+        <h4>Don't have an account ? </h4>
+        <h4>Sign Up</h4>
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
