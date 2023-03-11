7937e15aa5821e50a6b2165966de96854d7cb460
AmanApT <amanasstudent@gmail.com>
2023-03-11 11:52:29 +0530

    Started desgining Recommendar System

--- frontend/src/component/questions/Individual/Recommend.js
+++ frontend/src/component/questions/Individual/Recommend.js
@@ -6,34 +6,35 @@ const Recommend = ({ setToggleQuestion, opArr }) => {
   return (
     <div className="recMain">
       <div className="recChild">
-        <div>
-          <div onClick={() => setRecommendToggle(1)}>Electricity</div>
-          <div onClick={() => setRecommendToggle(2)}>Car Mileage</div>
-          <div onClick={() => setRecommendToggle(3)}>Newspapers</div>
-          <div onClick={() => setRecommendToggle(4)}>Aluminium & Tin</div>
+        <div className="recLeft">
+          <h3 onClick={() => setRecommendToggle(1)}>Electricity</h3>
+          <h3 onClick={() => setRecommendToggle(2)}>Car Mileage</h3>
+          <h3 onClick={() => setRecommendToggle(3)}>Newspapers</h3>
+          <h3 onClick={() => setRecommendToggle(4)}>Aluminium & Tin</h3>
         </div>
         <div className="recProgressBar"></div>
+        <div className="recRight">
         {recommendToggle === 1 ? (
           <div>
             <h1>Electricity Bill</h1>
-            <div>{opArr[1]}</div>
+            {/* <div>{opArr[1]}</div> */}
           </div>
         ) : recommendToggle === 2 ? (
           <div>
             <h1>Car Mileage</h1>
-            <div>{opArr[2]}</div>
+            {/* <div>{opArr[2]}</div> */}
           </div>
         ) : recommendToggle === 3 ? (
           <div>
             <h1>Recycling Newspapers</h1>
-            <div>{opArr[3]}</div>
+            {/* <div>{opArr[3]}</div> */}
           </div>
         ) : (
           <div>
             <h1>Recycling Aluminium & Tin</h1>
-            <div>{opArr[4]}</div>
+            {/* <div>{opArr[4]}</div> */}
           </div>
-        )}
+        )} </div>
       </div>
       {/* <button onClick={() => setToggleQuestion(5)}> wapas aa ja </button> */}
     </div>
